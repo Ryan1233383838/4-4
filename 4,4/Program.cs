652b2720d01df8b4520293ef9b6f5438ba181650
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-12-19 15:26:01 +0100

    Add project files.

---
+++ 4,4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lösenord;
+            do
+            {
+                Console.WriteLine("Ange lösenord");
+                lösenord = Console.ReadLine();
+
+                if (lösenord != "hemligt")
+                {
+                    Console.WriteLine("fel lösenord, försök igen!");
+                }
+
+
+            } while (lösenord != "hemligt");
+            Console.WriteLine(" Korrekt lösenord! du är inloggad.");
+        }
+    }
+}
